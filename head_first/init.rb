30e45771026c5b0eb10376749014e4f64b376606
Vagner Zampieri <vgzampieri@gmail.com>
2011-12-04 15:20:18 -0200

    Add new project

---
+++ head_first/init.rb
@@ -0,0 +1,11 @@
+# Load Bundler
+require 'rubygems'
+require 'bundler/setup'
+
+### Duck ###
+# Launch this Ruby file from the command line to get started.
+
+APP_ROOT = File.dirname(__FILE__)
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+
