dc6c2226afb326d8e07bbf2ed86851c05a70d4e8
Vagner Zampieri <vgzampieri@gmail.com>
2014-10-16 19:27:07 -0300

    Adding documentation

--- ruby/food_finder/lib/support/number_helper.rb
+++ ruby/food_finder/lib/support/number_helper.rb
@@ -1,6 +1,27 @@
 # This module illustrates how additional functionality can be
 # included (or "mixed-in") to a class and then reused.
 # Borrows heavily from Ruby on Rails' number_to_currency method.
+# Use:
+# class Dollar
+#   include NumberHelper
+#   attr_accessor :number
+#
+#   def initialize(number)
+#       @number = number_to_currency(number)
+#   end
+# end
+#
+# class Real
+#   include NumberHelper
+#   attr_accessor :number
+#
+#   def initialize(number)
+#     @number = number_to_currency(number,
+#                                  unit: 'R$',
+#                                  delimiter: '.',
+#                                  separator: ',')
+#   end
+# end
 module NumberHelper
   def number_to_currency(number, options = {})
     unit      = options[:unit]      || '$'
