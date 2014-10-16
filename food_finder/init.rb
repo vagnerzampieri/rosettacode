60bc55f9e8345ed0a9a37dc9bb464d9bfaa4b8cb
Vagner Zampieri <vgzampieri@gmail.com>
2014-10-16 08:49:07 -0300

    Move file to ruby

--- food_finder/init.rb
+++
@@ -1,8 +0,0 @@
-### Food Finder ###
-# Launch this Ruby file from the command line to get started.
-
-require File.expand_path('../boot', __FILE__)
-require 'guide'
-
-guide = Guide.new('restaurants.txt')
-guide.launch!
--- food_finder/test/guide_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path('../../boot', __FILE__)
-require 'guide'
-
-class GuideTest < Test::Unit::TestCase
-  def test_load
-    puts "Load"
-  end
-end
