58eb9b2ee792c60a2e84fb44ee8e49e639eb5bf2
Vagner Zampieri <vgzampieri@gmail.com>
2014-10-18 20:47:34 -0300

    Refactoring number helper

--- ruby/food_finder/lib/support/number_helper.rb
+++ ruby/food_finder/lib/support/number_helper.rb
@@ -1,3 +1,4 @@
+require 'support/formats_in_currency'
 # This module illustrates how additional functionality can be
 # included (or "mixed-in") to a class and then reused.
 # Borrows heavily from Ruby on Rails' number_to_currency method.
@@ -24,31 +25,7 @@
 # end
 module NumberHelper
   def number_to_currency(number, options = {})
-    unit      = options[:unit]      || '$'
-    precision = options[:precision] || 2
-    delimiter = options[:delimiter] || ','
-    separator = options[:separator] || '.'
-
-    separator         = '' if precision == 0
-    integer, decimal  = number.to_s.split('.')
-
-    i = integer.length
-    until i <= 3
-      i -= 3
-      integer = integer.insert(i, delimiter)
-    end
-
-    if precision == 0
-      precise_decimal = ''
-    else
-      # make sure decimal is not nil
-      decimal ||= '0'
-      # make sure the decimal is not too large
-      decimal = decimal[0, precision - 1]
-      # make sure the decimal is not too short
-      precise_decimal = decimal.ljust(precision, '0')
-    end
-
-    unit + integer + separator + precise_decimal
+    format = FormatsInCurrency.new(number, options)
+    format.unit + format.number + format.separator + format.precise_decimal
   end
 end
--- ruby/food_finder/spec/support/number_helper_spec.rb
+++ ruby/food_finder/spec/support/number_helper_spec.rb
@@ -3,24 +3,24 @@ require 'support/number_helper'
 RSpec.describe NumberHelper do
   describe '#number_to_currency' do
     context 'to dollar' do
-      it 'with 2 digits' do
+      it 'with 2 digits should be $10.00' do
         dollar = Dollar.new(10)
         expect(dollar.number).to eql('$10.00')
       end
 
-      it 'with 6 digits' do
+      it 'with 6 digits should be $1,000.00' do
         dollar = Dollar.new(1000)
         expect(dollar.number).to eql('$1,000.00')
       end
     end
 
     context 'to real' do
-      it 'with 2 digits' do
+      it 'with 2 digits should be R$10,00' do
         real = Real.new(10)
         expect(real.number).to eql('R$10,00')
       end
 
-      it 'with 6 digits' do
+      it 'with 6 digits should be R$1.000,00' do
         real = Real.new(1000)
         expect(real.number).to eql('R$1.000,00')
       end
