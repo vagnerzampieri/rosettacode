0dfb60704addeb1d2542b13eb518ab3136c0d6ab
Vagner Zampieri <vgzampieri@gmail.com>
2014-10-20 20:20:25 -0200

    Load with env variable

--- ruby/food_finder/boot.rb
+++ ruby/food_finder/boot.rb
@@ -3,10 +3,10 @@ require 'rubygems'
 require 'bundler/setup'
 
 # Load paths
-APP_ROOT = File.dirname(__FILE__)
+ENV['APP_ROOT'] = File.dirname(__FILE__)
 
 load_paths = %w[lib spec data]
 
 load_paths.each do |path|
-  $:.unshift(File.join(APP_ROOT, path))
+  $:.unshift(File.join(ENV['APP_ROOT'], path))
 end
