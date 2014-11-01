aacc181bf256f1430ce40a176a9a439ba6eb02ae
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-01 21:36:40 +0100

    Refactoring

--- python/day_of_the_week/day_of_the_week.py
+++ python/day_of_the_week/day_of_the_week.py
@@ -4,8 +4,11 @@ from datetime import date
 def day_of_the_week():
     years = []
     for year in range(2008, 2121):
-        if date(year, 12, 25).strftime("%A") == "Sunday":
+        if is_sunday(year):
             years.append(year)
     return years
+
+def is_sunday(year):
+     return date(year, 12, 25).strftime("%A") == "Sunday"
 
 # print day_of_the_week()
