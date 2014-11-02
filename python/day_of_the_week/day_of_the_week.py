da2ae110bd27ff71ae63ea449f8c517af4a14bd9
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-02 20:12:58 +0100

    Fiz range

--- python/day_of_the_week/day_of_the_week.py
+++ python/day_of_the_week/day_of_the_week.py
@@ -3,12 +3,13 @@ from datetime import date
 
 def day_of_the_week():
     years = []
-    for year in range(2008, 2121):
+    for year in range(2008, 2122):
         if is_sunday(year):
             years.append(year)
     return years
 
+
 def is_sunday(year):
-     return date(year, 12, 25).strftime("%A") == "Sunday"
+    return date(year, 12, 25).strftime("%A") == "Sunday"
 
 # print day_of_the_week()
