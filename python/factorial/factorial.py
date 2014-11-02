ac822df7a7584a14c6ab76be820bdb7f46364766
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-02 21:19:27 +0100

    Refactoring

--- python/factorial/factorial.py
+++ python/factorial/factorial.py
@@ -1,10 +1,9 @@
+from operator import mul
+from functools import reduce
+
+
 def factorial(value):
-    fact = 1
-    array = range(1, value + 1)
-    array.reverse()
-    for number in array:
-        fact *= number
-    return fact
+    return reduce(mul, range(1, value+1), 1)
 
 # print factorial(3)
 # print factorial(2)
