670c08676f6ddd9752908c18fd82cb55342d95d8
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-03 01:04:10 +0100

    Refactoring

--- python/fibonacci/fibonacci.py
+++ python/fibonacci/fibonacci.py
@@ -4,10 +4,8 @@ def fibonacci(value):
     for i in range(value):
         length = len(array)
 
-        if i == 0:
-            array.append(0)
-        elif i == 1:
-            array.append(1)
+        if i <= 1:
+            array = [0, 1]
         else:
             last = array[length - 1]
             penultimate = array[length - 2]
