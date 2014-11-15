1cd9505df1e779df12bcfba8758b917862b055ec
Vagner Zampieri <vgzampieri@gmail.com>
2014-11-15 14:33:57 +0000

    Adding tests to factorial

--- r/factorial/tests/test_factorial.R
+++ r/factorial/tests/test_factorial.R
@@ -1,15 +1,5 @@
 test_that("Factorial", {
-  expect_that(1 ^ 1, equals(1))
-  expect_that(2 ^ 2, equals(4))
-
-  expect_that(2 + 2 == 4, is_true())
-  expect_that(2 == 1, is_false())
-
-  expect_that(1, is_a('numeric'))
-
-  expect_that(print('Hello World!'), prints_text('Hello World!'))
-
-  expect_that(log('a'), throws_error())
-
-  expect_that(factorial(16), takes_less_than(1))
+  expect_that(factorial(2), equals(2))
+  expect_that(factorial(3), equals(6))
+  expect_that(factorial(10), equals(3628800))
 })
