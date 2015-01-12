eec66db1ef66bc9f7c342b078b33b67641e22409
Vagner Zampieri <vgzampieri@gmail.com>
2015-01-12 10:45:17 -0200

    Test factorial 0

--- ruby/factorial/factorial.rb
+++ ruby/factorial/factorial.rb
@@ -2,4 +2,5 @@
 # factorial(10)
 # => 3628800
 def factorial(value)
+  return 1 if value == 0
 end
--- ruby/factorial/factorial_spec.rb
+++ ruby/factorial/factorial_spec.rb
@@ -2,6 +2,10 @@ require './factorial'
 
 RSpec.describe 'Factorial' do
   context 'when factorial is' do
+    it '0 equal 1' do
+      expect(factorial(0)).to eql(1)
+    end
+
     it '2 equal 2' do
       expect(factorial(2)).to eql(2)
     end
