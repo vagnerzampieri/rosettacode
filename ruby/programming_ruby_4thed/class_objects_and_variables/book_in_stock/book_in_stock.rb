23271e4664a3b7abbfd6a1cf1fac36cee2a89315
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-17 13:03:11 +0000

    Use to_s to modify print

--- ruby/programming_ruby_4thed/class_objects_and_variables/book_in_stock/book_in_stock.rb
+++ ruby/programming_ruby_4thed/class_objects_and_variables/book_in_stock/book_in_stock.rb
@@ -3,8 +3,16 @@ class BookInStock
     @isbn = isbn
     @price = Float(price)
   end
+
+  def to_s
+    "ISBN: #{@isbn}, price: #{@price}"
+  end
 end
 
 p b1 = BookInStock.new('isbn1', 3)
 p b2 = BookInStock.new('isbn2', 3.14)
 p b3 = BookInStock.new('isbn3', '5.67')
+
+puts b1
+puts b2
+puts b3
