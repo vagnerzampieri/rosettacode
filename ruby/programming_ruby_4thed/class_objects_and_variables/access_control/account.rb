77f84adc927841982f2bd58afd9e844d528d5bc3
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-17 22:05:46 +0000

    Adding protected method

--- ruby/programming_ruby_4thed/class_objects_and_variables/access_control/account.rb
+++ ruby/programming_ruby_4thed/class_objects_and_variables/access_control/account.rb
@@ -1,6 +1,14 @@
 class Account
   attr_accessor :balance
+  attr_reader   :cleared_balance
+  protected     :cleared_balance
+
   def initialize(balance)
-    @balance = balance
+    @balance          = balance
+    @cleared_balance  = balance
+  end
+
+  def greater_balance_than?(other)
+    @cleared_balance > other.cleared_balance
   end
 end
--- ruby/programming_ruby_4thed/class_objects_and_variables/access_control/main.rb
+++ ruby/programming_ruby_4thed/class_objects_and_variables/access_control/main.rb
@@ -6,3 +6,6 @@ p checking = Account.new(200)
 
 p trans = Transaction.new(checking, savings)
 p trans.transfer(50)
+
+p savings.greater_balance_than?(checking)
+p checking.greater_balance_than?(savings)
