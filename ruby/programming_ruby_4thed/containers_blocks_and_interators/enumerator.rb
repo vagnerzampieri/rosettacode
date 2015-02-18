bf119d31d1a600d47b16210d4ab09b7d837af7c5
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-18 13:54:23 +0000

    Lazy Enumerators

--- ruby/programming_ruby_4thed/containers_blocks_and_interators/enumerator.rb
+++ ruby/programming_ruby_4thed/containers_blocks_and_interators/enumerator.rb
@@ -56,3 +56,49 @@ p triangular_numbers
   .infinite_select { |val| val % 10 == 0 }
   .infinite_select { |val| val.to_s =~ /3/ }
   .first(5)
+
+p 'Lazy Enumerators in Ruby 2'
+
+def Integer.all
+  Enumerator.new do |yielder, n: 0|
+    loop { yielder.yield(n += 1) }
+  end.lazy
+end
+
+p Integer.all.first(10)
+
+p Integer
+  .all
+  .select { |i| (i % 3).zero? }
+  .first(10)
+
+def palindrome?(n)
+  n = n.to_s
+  n == n.reverse
+end
+
+p Integer
+  .all
+  .select { |i| (i % 3).zero? }
+  .select { |i| palindrome?(i) }
+  .first(10)
+
+multiple_of_three = Integer
+  .all
+  .select { |i| (i % 3).zero? }
+
+p multiple_of_three.first(10)
+
+m3_palindrome = multiple_of_three
+  .select { |i| palindrome?(i) }
+
+p m3_palindrome.first(10)
+
+multiple_of_three = -> n { (n % 3).zero? }
+palindrome = -> n { n = n.to_s; n == n.reverse }
+
+p Integer
+  .all
+  .select(&multiple_of_three)
+  .select(&palindrome)
+  .first(10)
