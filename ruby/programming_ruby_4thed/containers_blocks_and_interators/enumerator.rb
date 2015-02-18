1d63239f262c8925e0a0e632feadd78b6fceba27
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-18 00:00:33 +0000

    Create enumerator

--- ruby/programming_ruby_4thed/containers_blocks_and_interators/enumerator.rb
+++ ruby/programming_ruby_4thed/containers_blocks_and_interators/enumerator.rb
@@ -13,3 +13,31 @@ end
 loop do
   p long_enum.next
 end
+
+enum = 'cat'.enum_for(:each_char)
+p enum.to_a
+
+enum_in_threes = (1..10).enum_for(:each_slice, 3)
+p enum_in_threes.to_a
+
+triangular_numbers = Enumerator.new do |yielder|
+  number = 0
+  count = 1
+  loop do
+    number += count
+    count += 1
+    yielder.yield number
+  end
+end
+
+p triangular_numbers.first(5)
+
+def infinite_select(enum, &block)
+  Enumerator.new do |yielder|
+    enum.each do |value|
+      yielder.yield(value) if block.call(value)
+    end
+  end
+end
+
+p infinite_select(triangular_numbers) { |val| val % 10 == 0 }.first(5)
