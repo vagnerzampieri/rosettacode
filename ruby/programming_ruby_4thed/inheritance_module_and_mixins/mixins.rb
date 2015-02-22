100df9260a931118ab195c98a1e9a3098a3c880c
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-22 12:29:56 +0000

    Other example to mixins

--- ruby/programming_ruby_4thed/inheritance_module_and_mixins/mixins.rb
+++ ruby/programming_ruby_4thed/inheritance_module_and_mixins/mixins.rb
@@ -27,3 +27,31 @@ et = EightTrack.new('Surrealistic Pillow')
 
 p ph.who_am_i?
 p et.who_am_i?
+
+class Person
+  include Comparable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "#{@name}"
+  end
+
+  def <=>(other)
+    self.name <=> other.name
+  end
+end
+
+p1 = Person.new('Matz')
+p2 = Person.new('Guido')
+p3 = Person.new('Larry')
+
+if p1 > p2
+  puts "#{p1.name}'s name > #{p2.name}'s name"
+end
+
+puts 'Sorted List:'
+puts [p1, p2, p3].sort
