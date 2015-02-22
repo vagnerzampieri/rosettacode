b211c25c539dd4b01fad7eb8f16ce2091cf78290
Vagner Zampieri <vgzampieri@gmail.com>
2015-02-22 00:54:52 +0000

    Block anotation

--- ruby/programming_ruby_4thed/containers_blocks_and_interators/blocks_can_be_objects.rb
+++ ruby/programming_ruby_4thed/containers_blocks_and_interators/blocks_can_be_objects.rb
@@ -52,3 +52,11 @@ end
     -> { puts "#{val} is small" },
     -> { puts "#{val} is big" }
 end
+
+proc1 = lambda do |a, *b, &block|
+  puts "a = #{a.inspect}"
+  puts "b = #{b.inspect}"
+  block.call
+end
+
+proc1.call(1, 2, 3, 4) { puts 'in block1' }
