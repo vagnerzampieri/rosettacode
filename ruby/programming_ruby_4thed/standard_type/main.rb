936fb94fc576535d6408a5b11bf4d64ad94794b4
Vagner Zampieri <vgzampieri@gmail.com>
2015-03-08 23:18:26 +0000

    Split time

--- ruby/programming_ruby_4thed/standard_type/main.rb
+++ ruby/programming_ruby_4thed/standard_type/main.rb
@@ -5,7 +5,9 @@ File.open('songdata') do |song_file|
 
   song_file.each do |line|
     file, length, name, title = line.chomp.split(/\S*\|\s*/)
-    songs << Song.new(title, name, length)
+    name.squeeze!(" ")
+    mins, secs = length.scan(/\d+/)
+    songs << Song.new(title, name, mins.to_i*60 + secs.to_i)
   end
 
   puts songs[1]
