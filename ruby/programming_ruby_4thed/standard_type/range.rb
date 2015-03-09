a855a5e1cf9c566ec581f50f78798cbe5f50f853
Vagner Zampieri <vgzampieri@gmail.com>
2015-03-09 00:46:46 +0000

    Ranges as Intervals

--- ruby/programming_ruby_4thed/standard_type/range.rb
+++ ruby/programming_ruby_4thed/standard_type/range.rb
@@ -22,3 +22,18 @@ p1 = PowerOfTwo.new(4)
 p2 = PowerOfTwo.new(32)
 
 puts (p1..p2).to_a
+
+puts "Ranges as Intervals"
+car_age =  gets.to_f
+case car_age
+when 0..1
+  puts 'Mmm.. new car smell'
+when 1..3
+  puts 'Nice and new'
+when 3..10
+  puts 'Reliable but slightly dinged'
+when 10..30
+  puts 'Clunker'
+else
+  puts 'Vintage gem'
+end
