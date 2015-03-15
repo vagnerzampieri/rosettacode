55d25d17f11f3ee79218e9bfef939c5552d9bf8c
Vagner Zampieri <vgzampieri@gmail.com>
2015-03-15 23:02:37 +0000

    Parsing HTML with nokogiri

--- ruby/programming_ruby_4thed/basic_input_and_output/parsing_html.rb
+++ ruby/programming_ruby_4thed/basic_input_and_output/parsing_html.rb
@@ -1,3 +1,5 @@
+p "open-uri ========================"
+
 require 'open-uri'
 
 page = open('https://pragprog.com/titles/ruby3/programming-ruby-1-9').read
@@ -5,3 +7,16 @@ page = open('https://pragprog.com/titles/ruby3/programming-ruby-1-9').read
 if page =~ %r{<title>(.*?)</title>}m
   puts "Title is #{$1.inspect}"
 end
+
+p "nokogiri ========================"
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open('https://pragprog.com/'))
+
+puts 'Page title is ' + doc.xpath('//title').inner_html
+
+puts doc.css('div#copyright p')
+
+puts "\nSecond hyperlink is"
+puts doc.xpath('id("site-links")//a[2]')
+puts doc.css('#site-links a:nth-of-type(2)')
