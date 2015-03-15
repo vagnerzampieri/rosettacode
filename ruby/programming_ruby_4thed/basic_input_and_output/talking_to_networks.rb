851ccf329dfc15a636cc53eed335710a27829398
Vagner Zampieri <vgzampieri@gmail.com>
2015-03-15 22:35:21 +0000

    Talking to networks with open-uri

--- ruby/programming_ruby_4thed/basic_input_and_output/talking_to_networks.rb
+++ ruby/programming_ruby_4thed/basic_input_and_output/talking_to_networks.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 
+p 'With net/http =========='
 uri = URI('https://pragprog.com/book/ruby3/programming-ruby-1-9')
 response = Net::HTTP.get_response(uri)
 
@@ -7,4 +8,11 @@ p response
 
 if response.is_a?(Net::HTTPOK)
   puts response.body.scan(/<img alt=".*?" src="(.*?)"/m).uniq[0,3]
+end
+
+require 'open-uri'
+
+p 'With open-uri =========='
+open('https://pragprog.com') do |f|
+  puts f.read.scan(/<img alt=".*?" src="(.*?)"/m).uniq[0,3]
 end
