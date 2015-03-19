c06d3d2d5801ab4510e67e3bd653e78d088645ca
Vagner Zampieri <vgzampieri@gmail.com>
2015-03-19 00:43:27 +0000

    Adding threads

---
+++ ruby/programming_ruby_4thed/fibers_threads_and_processes/threads.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+
+pages = %w( www.rubycentral.org slashdot.org www.google.com )
+
+puts "With thread ======================="
+
+threads = pages.map do |page_to_fetch|
+  Thread.new(page_to_fetch) do |url|
+    http = Net::HTTP.new(url, 80)
+    print "Fetching: #{url}\n"
+    resp = http.get('/')
+    print "Got #{url}: #{resp.message}\n"
+  end
+end
+
+threads.each { |thr| thr.join }
+
+puts "Without thread ======================="
+
+pages.map do |url|
+  http = Net::HTTP.new(url, 80)
+  print "Fetching: #{url}\n"
+  resp = http.get('/')
+  print "Got #{url}: #{resp.message}\n"
+end
+
