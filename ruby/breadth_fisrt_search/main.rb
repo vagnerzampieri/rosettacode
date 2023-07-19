b206205e3d3cd9176ac71946ba24419e0ea83f19
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-19 18:03:34 -0300

    add breadth first search

---
+++ ruby/breadth_fisrt_search/main.rb
@@ -0,0 +1,35 @@
+GRAFO = {
+  you: %w[alice bob claire],
+  bob: %w[anuj peggy],
+  alice: ['peggy'],
+  claire: %w[thom jonny],
+  anuj: [],
+  peggy: [],
+  thom: [],
+  jonny: []
+}.freeze
+
+def person_is_seller(name)
+  name[-1] == 'm'
+end
+
+def search(name)
+  search_queue = []
+  search_queue += GRAFO[name.to_sym]
+  searched = []
+
+  while search_queue.any?
+    person = search_queue.shift
+    next if searched.include?(person)
+
+    if person_is_seller(person)
+      puts "#{person} is a seller!"
+      return true
+    end
+
+    search_queue += GRAFO[person.to_sym]
+    searched << person
+  end
+end
+
+search('alice')
