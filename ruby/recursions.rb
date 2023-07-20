f6b648d26d800fd72aa5862875146bb655a9c3e5
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-20 08:54:25 -0300

    add recursions

--- ruby/recursions.rb
+++ ruby/recursions.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
 # table test
-# array = [1, 2, 3, 4, 5]
-# array = [2, 3, 4, 5]
-# array = [3, 4, 5]
-# array = [4, 5]
-# array = [5]
+# array = [1, 2, 3]
+# array = [2, 3]
+# array = [3]
 # array = []
-# 5 + 0 = 5
-# 4 + 5 = 9
-# 3 + 9 = 12
-# 2 + 12 = 14
-# 1 + 14 = 15
+# 3 + 0 = 3
+# 2 + 3 = 5
+# 1 + 5 = 6
+# 6 + 0 = 6
 def recursion_sum(array)
   return 0 if array.empty?
 
   array[0] + recursion_sum(array[1..])
 end
+
+p recursion_sum([1, 2, 3])
 
 p recursion_sum([1, 2, 3, 4, 5])
 
