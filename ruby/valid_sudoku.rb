3185a4f9f507e35d08e7e77179a19de16c53b9e1
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-21 12:55:12 -0300

    add valid_sudoku

--- ruby/valid_sudoku.rb
+++ ruby/valid_sudoku.rb
@@ -11,21 +11,53 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def valid_sudoku?(board)
+  hash_column = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+
+  divide_into_subgrids(board).each do |row|
+    hash_row = Hash.new(0)
+
+    row.each do |rw|
+      hash_row[rw] += 1
+      return false if hash_row[rw] > 1
+    end
+  end
+
   board.each do |row|
     hash_row = Hash.new(0)
-    hash_column = Hash.new(Hash.new(0))
 
     row.each.with_index do |rw, i|
       next if rw == '.'
 
       hash_row[rw] += 1
       hash_column[i][rw] += 1
-      return rw if hash_column[i][rw] > 1
+
       return false if hash_row[rw] > 1
+      return false if hash_column[i][rw] > 1
     end
   end
 
   true
+end
+
+def divide_into_subgrids(board)
+  subgrids = []
+
+  (0..6).step(3) do |row_start|
+    (0..6).step(3) do |col_start|
+      subgrid = []
+
+      3.times do |row_offset|
+        3.times do |col_offset|
+          value = board[row_start + row_offset][col_start + col_offset]
+          subgrid << value unless value == '.'
+        end
+      end
+
+      subgrids << subgrid
+    end
+  end
+
+  subgrids
 end
 
 board = [
@@ -40,6 +72,7 @@ board = [
   ['.', '.', '.', '.', '8', '.', '.', '7', '9']
 ]
 p valid_sudoku?(board)
+# p divide_into_subgrids(board)
 # true
 
 board = [
