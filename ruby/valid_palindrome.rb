c4d99bb32f59523a8439df487f55d6b20138a86c
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-24 11:42:09 -0300

    add valid_palindrome

---
+++ ruby/valid_palindrome.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def valid_palindrome(s)
+  modified_s = s.downcase.gsub(/[^a-z0-9]/, '')
+
+  modified_s == modified_s.reverse
+end
+
+s = 'A man, a plan, a canal: Panama'
+p valid_palindrome(s)
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+s = 'race a car'
+p valid_palindrome(s)
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+s = ' '
+p valid_palindrome(s)
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
