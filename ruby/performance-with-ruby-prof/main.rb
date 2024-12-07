b07263411b67d2cc33f033aadcb50bd7d1412cde
Vagner Zampieri <vagner.zampieri@nuuvem.com>
2024-12-07 10:38:12 -0300

    Update everything

--- ruby/performance-with-ruby-prof/main.rb
+++ ruby/performance-with-ruby-prof/main.rb
@@ -1,9 +1,16 @@
-require 'ruby-prof'
+# require 'ruby-prof'
+require 'vernier'
 
 def sleeper
-  sleep(4)
+  # sleep(4)
+  t = Time.now.to_f
+  while Time.now.to_f < t + 4.0
+    # do nothing
+  end
 end
 
-result = RubyProf::Profile.profile { sleeper }
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(STDOUT)
+# result = RubyProf::Profile.profile { sleeper }
+# printer = RubyProf::FlatPrinter.new(result)
+# printer.print(STDOUT)
+
+Vernier.profile(out: 'sleeper.json') { sleeper }
